9e9b6e4b91e482bd707a72a6cd962d8a5aa77314
AyoubJedidi <ayoub.ejedidi@gmail.com>
2025-11-25 16:10:56 +0100

    Add buildStage function

---
+++ vars/buildStage.groovy
@@ -0,0 +1,28 @@
+def call(Map config) {
+    def projectType = config.projectType
+    
+    echo "🔨 Building ${projectType} project..."
+    
+    switch(projectType) {
+        case 'maven':
+            sh 'mvn clean install -DskipTests'
+            break
+            
+        case 'gradle':
+            sh './gradlew build -x test --no-daemon'
+            break
+            
+        case 'npm':
+            sh 'npm install && npm run build'
+            break
+            
+        case 'dotnet':
+            sh 'dotnet build --configuration Release'
+            break
+            
+        default:
+            error "Unsupported project type: ${projectType}"
+    }
+    
+    echo "✓ Build completed successfully"
+}
