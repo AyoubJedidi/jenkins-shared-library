b405d971374b0bdc74ba927257f4468c000aa20a
AyoubJedidi <ayoub.ejedidi@gmail.com>
2025-11-27 15:39:19 +0100

    fixing buildstage.groovy

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,31 +1,30 @@
-def call(Map config) {
-    def projectType = config.projectType
+    def call(Map config) {
+        def projectType = config.projectType
 
-    // Ensure the actual project repo is checked out
-git branch: 'main', url: 'https://github.com/AyoubJedidi/jenkins-shared-library'
+        // Ensure the actual project repo is checked out
 
-    echo "🔨 Building ${projectType} project..."
+        echo "🔨 Building ${projectType} project..."
 
-    switch(projectType) {
-        case 'maven':
-            sh 'mvn clean install -DskipTests'
-            break
+        switch(projectType) {
+            case 'maven':
+                sh 'mvn clean install -DskipTests'
+                break
 
-        case 'gradle':
-            sh './gradlew build -x test --no-daemon'
-            break
+            case 'gradle':
+                sh './gradlew build -x test --no-daemon'
+                break
 
-        case 'npm':
-            sh 'npm install && npm run build'
-            break
+            case 'npm':
+                sh 'npm install && npm run build'
+                break
 
-        case 'dotnet':
-            sh 'dotnet build --configuration Release'
-            break
+            case 'dotnet':
+                sh 'dotnet build --configuration Release'
+                break
 
-        default:
-            error "Unsupported project type: ${projectType}"
+            default:
+                error "Unsupported project type: ${projectType}"
+        }
+
+        echo "✓ Build completed successfully"
     }
-
-    echo "✓ Build completed successfully"
-}
